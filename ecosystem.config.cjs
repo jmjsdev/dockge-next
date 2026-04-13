b2d05e7d3512c4912a51bbc4b722d40b41943eb6
Jmjsdec <arnaudgs@gmail.com>
2026-04-13 11:52:04 +0200

    fix
    Signed-off-by: Jmjsdec <arnaudgs@gmail.com>

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -8,13 +8,10 @@ module.exports = {
             cwd: "/home/arnaud/dev/dockge-next",
             env: {
                 NODE_ENV: "production",
+                DOCKGE_DATA_DIR: "/home/arnaud/dev/datas/dockge/data",
                 DOCKGE_STACKS_DIR: "/home/arnaud/docker",
                 DOCKGE_ENABLE_CONSOLE: "true",
             },
-            watch: false,
-            max_memory_restart: "512M",
-            restart_delay: 3000,
-            exp_backoff_restart_delay: 1000,
-        }
-    ]
+        },
+    ],
 };
